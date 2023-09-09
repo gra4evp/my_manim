c56438325f43bcf844df5cbb13203dfad9c44dad
gra4evp <petrgrachev1@gmail.com>
2023-09-09 17:03:03 +0300

    ...

--- presentation_tutorial/matrix_scene.py
+++ presentation_tutorial/matrix_scene.py
@@ -2,74 +2,17 @@ from manim import *
 import numpy as np
 import random
 
-col1 = '#B1CE96'
-col2 = '#EA8E8B'
+
+# manim -pql -i -r 2000,2000 matrix_scene.py MinkowskiNorm
 
 
 config.background_color = WHITE
 
+config.frame_width = 6
+config.frame_height = 6
 
-class MatrixSquare(MovingCameraScene):
-    def construct(self):
-        squares = VGroup()
-        for i in range(10):
-            for j in range(8):
-                square = Square(0.7)
-                square.shift(i * RIGHT*0.8, j * UP*0.8)
-                square.set_stroke(BLACK)
-                squares.add(square)
-        squares.move_to(ORIGIN)
-
-        nums = VGroup()
-        for square in squares:
-            num = round(np.random.randint(0, 5)*np.random.random(), 1)
-            if num:
-                num = MathTex(f'{num}', color=BLACK, font_size=33)
-                square.set_fill(col1, 1)
-            else:
-                num = MathTex(r'\times', color=BLACK, font_size=50)
-                square.set_fill(col2, 1)
-            num.move_to(square)
-            nums.add(num)
-
-        self.add(squares, nums)
-        self.wait()
-
-        for k in range(10):
-            nums_to_rewrite = []
-            for _ in range(20):
-                index = np.random.randint(0, len(squares) - 1)
-                num = round(np.random.randint(0, 5) * np.random.random(), 1)
-                if num:
-                    num = MathTex(f'{num}', color=BLACK, font_size=33)
-                    squares[index].set_fill(col1, 1)
-                else:
-                    num = MathTex(r'\times', color=BLACK, font_size=50)
-                    squares[index].set_fill(col2, 1)
-                num.move_to(squares[index])
-                nums[index].replace(num)
-                nums_to_rewrite.append(FadeIn(nums[index]))
-            self.play(*nums_to_rewrite, run_time=1)
-
-        # counter = 0
-        # while counter <= 10:
-        #     index1 = np.random.randint(0, len(squares) - 1)
-        #     index2 = np.random.randint(0, len(squares) - 1)
-        #
-        #     if index1 != index2:
-        #         sq1 = squares[index1]
-        #         sq2 = squares[index2]
-        #
-        #         num1 = nums[index1]
-        #         num2 = nums[index2]
-        #         self.play(
-        #             sq1.animate.move_to(sq2),
-        #             num1.animate.move_to(sq2),
-        #             sq2.animate.move_to(sq1),
-        #             num2.animate.move_to(sq1),
-        #             run_time=0.8)
-        #
-        #         counter += 1
+# config.pixel_width = 1080
+# config.frame_height = 1080
 
 
 def generate_matrix(rows, cols):
@@ -95,17 +38,15 @@ class MatrixNumbers(Scene):
 
 class MinkowskiNorm(Scene):
     def construct(self):
-        self.camera.frame_width = 15
-        self.camera.frame_height = 9
 
-        grid = NumberPlane(x_range=(-3, 3), y_range=(-3, 3), axis_config={"color": BLACK})
+        grid = NumberPlane(x_range=(-2, 2), y_range=(-2, 2), axis_config={"color": BLACK})
 
         p_num = ValueTracker(0.3)
 
-        func = ImplicitFunction(lambda x, y: abs(x)**p_num.get_value() + abs(y)**p_num.get_value() - 1, color=BLACK)
+        func = ImplicitFunction(lambda x, y: abs(x)**p_num.get_value() + abs(y)**p_num.get_value() - 1, color=RED)
         func.add_updater(
             lambda m: m.become(
-                ImplicitFunction(lambda x, y: abs(x)**p_num.get_value() + abs(y)**p_num.get_value() - 1, color=BLACK)
+                ImplicitFunction(lambda x, y: abs(x)**p_num.get_value() + abs(y)**p_num.get_value() - 1, color=RED)
             )
         )
 
@@ -125,14 +66,14 @@ class MinkowskiNorm(Scene):
             sqrt = r"\sqrt[" + f"{p}" + "]"
             brc1 = r"|x_{1}|^{"+f"{p}"+"}"
             brc2 = r"|x_{2}|^{"+f"{p}"+"}"
-            return mobject.become(MathTex(sqrt + "{" + brc1 + " + " + brc2 + "} = 1", color=BLACK).shift(DOWN*3.5))
+            return mobject.become(MathTex(sqrt + "{" + brc1 + " + " + brc2 + "} = 1", color=RED).shift(DOWN*2.5))
 
         text_down.add_updater(updater_tex_formula)
 
         self.add(grid, text_down, func)
-        # self.play(p_num.animate.set_value(1), run_time=2)
-        # self.wait(0.1)
-        # self.play(p_num.animate.set_value(2), run_time=2)
-        # self.wait(0.1)
-        # self.play(p_num.animate.set_value(5), run_time=3)
-        # self.play(p_num.animate.set_value(0.3), run_time=3)
+        self.play(p_num.animate.set_value(1), run_time=2)
+        self.wait(0.1)
+        self.play(p_num.animate.set_value(2), run_time=2)
+        self.wait(0.1)
+        self.play(p_num.animate.set_value(5), run_time=3)
+        self.play(p_num.animate.set_value(0.3), run_time=3)
